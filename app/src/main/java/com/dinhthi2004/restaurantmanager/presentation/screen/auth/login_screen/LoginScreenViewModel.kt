eb23cc30a6edd8cb30b8fd009f2508f2bbb55125
Coammcam <hungnguyenisme84@gmail.com>
2024-10-09 01:19:55 +0700

    Add a login function using the API

--- app/src/main/java/com/dinhthi2004/restaurantmanager/presentation/screen/auth/login_screen/LoginScreenViewModel.kt
+++ app/src/main/java/com/dinhthi2004/restaurantmanager/presentation/screen/auth/login_screen/LoginScreenViewModel.kt
@@ -1,24 +1,28 @@
 package com.dinhthi2004.restaurantmanager.presentation.screen.auth.login_screen.viewmodel
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.dinhthi2004.restaurantmanager.presentation.screen.auth.login_screen.data.Account
-import com.dinhthi2004.restaurantmanager.presentation.screen.auth.login_screen.data.getSampleAccounts
+import com.dinhthi2004.restaurantmanager.model.Account
 import kotlinx.coroutines.launch
+import com.dinhthi2004.restaurantmanager.api.HttpReq
+import com.dinhthi2004.restaurantmanager.model.TokenManager
 
 class LoginViewModel : ViewModel() {
+    var api = HttpReq.getInstance()
 
     var emailError = mutableStateOf(false)
     var passwordError = mutableStateOf(false)
     var emptyEmailError = mutableStateOf(false)
     var emptyPasswordError = mutableStateOf(false)
     var invalidCredentialsError = mutableStateOf(false)
+    var isLoading = mutableStateOf(false)
 
-    fun handleLogin(email: String, password: String, onLoginSuccess: (String) -> Unit) {
+    fun handleLogin(username: String, password: String, onLoginSuccess: (String) -> Unit) {
         resetErrors()
 
-        if (email.isBlank()) {
+        if (username.isBlank()) {
             emptyEmailError.value = true
         }
         if (password.isBlank()) {
@@ -26,30 +30,45 @@ class LoginViewModel : ViewModel() {
         }
 
         if (!emptyEmailError.value && !emptyPasswordError.value) {
-            val account = validateAccount(email, password)
-            if (account != null) {
-                emailError.value = false
-                passwordError.value = false
-                invalidCredentialsError.value = false
+            isLoading.value = true;
 
-                viewModelScope.launch {
-                    val route = when (account.role) {
-                        "Admin" -> "home_admin_screen"
-                        "Manager" -> "BottomNavigation"
-                        "Waiter" -> "WAITER_MAIN_SCREEN"
-                        else -> "home_user_screen"
+            // Login
+            viewModelScope.launch {
+                try {
+                    val response = api.login(Account(username = username, password = password, role = 0, _id = ""))
+
+                    if (response.isSuccessful && response.body() != null) {
+                        val account = response.body()!!
+
+                        invalidCredentialsError.value = false;
+                        emailError.value = false
+                        passwordError.value = false
+
+                        val token = account.token
+                        if (token != null) {
+                            Log.d("token", token)
+                            TokenManager.token = token // Global token
+                        }
+
+                        val route = when (account.role) {
+                            0 -> "home_admin_screen" // admin
+                            1 -> "BottomNavigation" // manager
+                            2 -> "WAITER_MAIN_SCREEN" // waiter
+                            else -> "home_user_screen"
+                        }
+                        onLoginSuccess(route)
+                    } else {
+                        invalidCredentialsError.value = true
+
                     }
-                    onLoginSuccess(route)
+                } catch (e: Exception) {
+                    invalidCredentialsError.value = true
+                    Log.e("LoginViewModel", "Exception: ${e.message}", e)
+                } finally {
+                    isLoading.value = false
                 }
-            } else {
-                invalidCredentialsError.value = true
             }
         }
-    }
-
-    private fun validateAccount(email: String, password: String): Account? {
-        val accounts = getSampleAccounts()
-        return accounts.find { it.email == email && it.password == password }
     }
 
     private fun resetErrors() {
